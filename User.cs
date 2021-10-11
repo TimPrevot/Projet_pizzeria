fa31532fd6d66e88546cd7d671301e53f73743f8
marin <marin.van-remoortel@efrei.net>
2021-10-11 21:56:31 +0200

    getter setter class User

--- User.cs
+++ User.cs
@@ -19,5 +19,61 @@ namespace Projet_pizzeria
         {
             this.user_id = count++;
         }
+
+        public string GetFirstName() {
+            return this.firstName;
+        }
+
+        public string SetFirstName() {
+            this.firstName = FirstName;
+        }
+
+        public string GetLastName() {
+            return this.lastName;
+        }
+
+        public string SetLastName() {
+            this.lastName = LastName;
+        }
+
+        public string GetTel(){
+            return this.tel;
+        }
+
+        public string SetTel(){
+            this.tel = Tel;
+        }
+
+        public string GetAddress(){
+            return this.address;
+        }
+
+        public string SetAddress(){
+            this.address = Address;
+        }
+
+        public string GetCity(){
+            return this.city;
+        }
+
+        public string SetCity(){
+            this.city = City;
+        }
+
+        public string GetPostalCode(){
+            return this.postalCode;
+        }
+
+        public string SetPostalCode(){
+            this.postalCode = PostalCode;
+        }
+
+        public string GetEntity(){
+            return this.entity;
+        }
+
+        public string SetEntity(){
+            this.entity = Entity;
+        }
     }
 }
