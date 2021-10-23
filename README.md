a263db3de04bce4f9024f8213dc62f36420537b1
deadbang3r <timothe.prevot@efrei.net>
2021-10-23 23:03:55 +0200

    Added SQL scripts + README.md

--- README.md
+++ README.md
@@ -6,3 +6,17 @@
 - To create the database, run the sql script `create_db.sql`
 - To populate the database, run the sql script `populate.sql`
 - Change the `postgreConStr` value in the `App.cs` with your postgreSQL credentials
+
+###
+
+#####To create a new root user
+
+- The first time you run the app, immediately select "add a new user", then choose `entity = 2` to be asked to enter a username and a password.
+- Then exit the app and un-comment the lines 955 to 958 in the `App.cs` file, they look like this:
+```
+while (!isConnected)
+{
+    isConnected = myApp.connectClerk();
+}
+```
+- You can now login with your newly created credentials !
---
+++ populate-db.sql
@@ -0,0 +1,22 @@
+﻿INSERT INTO entity VALUES (default, 'clerk');
+INSERT INTO entity VALUES (default, 'client');
+INSERT INTO entity VALUES (default, 'driver');
+
+INSERT INTO sizes VALUES (default, 'm');
+INSERT INTO sizes VALUES (default, 'l');
+INSERT INTO sizes VALUES (default, 'xl');
+
+INSERT INTO types VALUES (default, 'pizza');
+INSERT INTO types VALUES (default, 'drink');
+
+INSERT INTO menu VALUES (default, 1, 12, 'Margherita', 1, true);
+INSERT INTO menu VALUES (default, 1, 14, 'Margherita', 2, true);
+INSERT INTO menu VALUES (default, 1, 15, 'Tartufata', 1, true);
+INSERT INTO menu VALUES (default, 1, 18, 'Tartufata', 2, true);
+INSERT INTO menu VALUES (default, 1, 13, 'Regina', 1, true);
+INSERT INTO menu VALUES (default, 1, 16, 'Regina', 2, true);
+INSERT INTO menu VALUES (default, 2, 3, 'Coca-cola', 1, true);
+INSERT INTO menu VALUES (default, 2, 3, 'Ice-Tea', 1, true);
+
+INSERT INTO users VALUES (default, 'firstclient', 'test', 'test', 'test', 'test', 'test', 2, null, null, null);
+INSERT INTO users VALUES (default, 'firstdriver', 'test2', 'test2', 'test2', 'test2', 'test2', 3, null, null, true);
